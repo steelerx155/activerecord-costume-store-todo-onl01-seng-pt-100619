f446dd4e850f47acadc97f4918fb7e1a46e58721
Randy Steele <steelerx155@gmail.com>
2019-12-23 20:35:10 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 3) do
   create_table "haunted_houses", force: :cascade do |t|
     t.string "name"
     t.string "location"
+    t.string "theme"
     t.float "price"
     t.boolean "family_friendly"
     t.date "opening_date"
